227c440da9ccd7369a105fef50a9ba6336302298
stevenheroku <herokujeff@gmail.com>
2021-02-17 17:11:50 -0600

    version 1

---
+++ JEFFERSONRR/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JEFFERSONRR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("NOMBRES");
+            Console.WriteLine("JEFFERSON STEVEN RODRIGUEZ RUEDA");
+            Console.WriteLine("JOSE ANDRE RUEDA ALVARAADO");
+
+        }
+    }
+}
